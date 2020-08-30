027a9499dd9a94e2b4dfdde70744f7e3c2ffd170
Josh Raiborde <joshsrai@yahoo.com>
2020-08-30 20:09:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,3 +11,7 @@ VALUES (1, "Aslan", "roar", "Lion", 1, 1),
 (6, "Aragorn", "Not idly do the leaves of Lorien fall", "Dunedain", 2,2),
 (7, "Boromir", "One does not simply walk into Mordor", "Human", 2, 2),
 (8, "Shelob", "...", "Giant Spider", 2,2);
+
+INSERT INTO character_books (book_id, character_id)
+VALUES
+(1,1), (1,2), (1,4), (2,1), (2,2), (3,1), (3,2), (3,3), (4,5), (4,6), (4,7), (5,5), (5,6), (6,5), (6,6), (6,8);
