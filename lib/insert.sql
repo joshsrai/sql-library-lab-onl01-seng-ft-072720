c3d0ede586c6e8ea3b5853ffba0e8d946fa8a57b
Josh Raiborde <joshsrai@yahoo.com>
2020-08-30 20:04:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,3 +3,8 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Chronicles of
 INSERT INTO books (id, title, year, series_id) VALUES (1, "The Lion, the Witch, and the Wardrobe", 1950, 1), (2, "Prince Caspian", 1951, 1), (3, "Voyage of the Dawn Treader", 1952, 1);
 INSERT INTO books (id, title, year, series_id) VALUES (4, "The Fellowship of the Ring", 1954, 2), (5, "The Two Towers", 1954, 2), (6, "The Return of the King", 1955, 2);
 
+INSERT INTO characters (id, name, motto, species, author_id, series_id)
+VALUES (1, "Aslan", "roar", "Lion", 1, 1),
+(2, "Lucy Pevensie", "Queen of Narnia", "Human", 1, 1),
+(3, "Eustace Scrubb", "It's not fair", "Human", 1, 1),
+(4, "Jadis", "Tomorrow, we will take Narnia forever", "Jinn/Giant", 1, 1)
