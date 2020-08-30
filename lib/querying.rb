98d3b8ed55c58ba64c5365e0053637e3abbbbec5
Josh Raiborde <joshsrai@yahoo.com>
2020-08-30 20:28:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,13 +37,27 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT 
-  
+  "SELECT series.title
+  FROM series
+  JOIN books
+  ON books.series_id = series.id
+  JOIN character_books 
+  ON books.id = book_id 
+  JOIN characters 
+  ON characters.id = character_books.character_id
+  WHERE chracters.species = 'human'
+  GROUP BY series.title 
+  LIMIT 1
   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT 
+  "SELECT characters.name, COUNT(*)
+  FROM characters 
+  JOIN character_books 
+  ON characters.id = character_books.character_id 
+  GROUP BY characters.name 
+  ORDER BY COUINT(*) DESC
   
   "
 end
